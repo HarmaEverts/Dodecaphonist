fd469f5de305d03eecfb7aa8c7c6ee0b9fbc79a1
Harma Everts <82085008+Meilindis@users.noreply.github.com>
2022-09-28 16:15:24 +0200

    Added signals/slots for toggles, slider.

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from PySide6.QtWidgets import (
     QApplication
-, QDialog
-, QMessageBox
-, QWidget
-, QLabel
-, QPushButton
-, QVBoxLayout
-, QLineEdit
-, QComboBox
-, QRadioButton
 , QMainWindow
 )
-from PySide6 import QtCore, QtWidgets, QtGui
 import sys
 from ui_Compunist import Ui_MainWindow
 
@@ -23,18 +13,18 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         # Set up the UI options in drop-downs etc.
-        self.ui.composition_mode.addItems(["Schoenberg", "Webern", "Stravinsky", "Custom"])
+        self.ui.composition_mode.addItems(["Schoenberg", "Webern", "Stravinsky", "Start from scratch"])
         self.ui.key.addItems(["Sharps", "Flats"])
-        self.ui.repeat_current_note.addItems(["Yes", "No"])
-        self.ui.repeat_previous_note.addItems(["Yes", "No"])
         enumerator_options = range(1, 13)
         denominator_options = [1, 2, 4, 8, 16, 32]
         self.ui.time_enumerator.addItems([str(x) for x in enumerator_options])
         self.ui.time_denominator.addItems([str(x) for x in denominator_options])
 
-        self.ui.notes_rests_slider.valueChanged.connect(self.valuechange)
+        self.ui.notes_rests_slider.valueChanged.connect(self.notes_rests_valuechange)
+        self.ui.repeat_current_note.stateChanged.connect(self.toggle_repeat_current_note)
+        self.ui.repeat_previous_note.stateChanged.connect(self.toggle_repeat_previous_note)
 
-    def valuechange(self):
+    def notes_rests_valuechange(self):
         """
         Checks the value of the notes - rests slider, and updates the notes and rests percentage labels accordingly.
         The total should always sum up to 100%
@@ -43,6 +33,22 @@ class MainWindow(QMainWindow):
         rests_value = 100 - notes_value
         self.ui.percent_notes.setText(str(notes_value) + " % notes")
         self.ui.percent_rests.setText(str(rests_value) + " % rests")
+
+    def toggle_repeat_current_note(self):
+        """
+        Toggles the repeat current note details depending on whether the checkbox is selected.
+        """
+        self.ui.currentlabel.setEnabled(self.ui.repeat_current_note.isChecked())
+        self.ui.currentslider.setEnabled(self.ui.repeat_current_note.isChecked())
+        self.ui.currentpercentage.setEnabled(self.ui.repeat_current_note.isChecked())
+
+    def toggle_repeat_previous_note(self):
+        """
+        Toggles the repeat previous note details depending on whether the checkbox is selected.
+        """
+        self.ui.previouslabel.setEnabled(self.ui.repeat_previous_note.isChecked())
+        self.ui.previousslider.setEnabled(self.ui.repeat_previous_note.isChecked())
+        self.ui.previouspercentage.setEnabled(self.ui.repeat_previous_note.isChecked())
 
 
 if __name__ == "__main__":
--- ui_Compunist.py
+++ ui_Compunist.py
@@ -15,7 +15,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
     QGroupBox, QHBoxLayout, QLabel, QLineEdit,
     QMainWindow, QPushButton, QSizePolicy, QSlider,
     QSpacerItem, QSpinBox, QStatusBar, QVBoxLayout,
@@ -58,25 +58,35 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.repeat_current_note = QComboBox(self.groupBox)
+        self.repeat_current_note = QCheckBox(self.groupBox)
         self.repeat_current_note.setObjectName(u"repeat_current_note")
 
         self.horizontalLayout_3.addWidget(self.repeat_current_note)
 
-        self.label_13 = QLabel(self.groupBox)
-        self.label_13.setObjectName(u"label_13")
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.horizontalLayout_3.addWidget(self.label_13)
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_3)
 
-        self.repeat_current_node_chance = QLineEdit(self.groupBox)
-        self.repeat_current_node_chance.setObjectName(u"repeat_current_node_chance")
+        self.currentlabel = QLabel(self.groupBox)
+        self.currentlabel.setObjectName(u"currentlabel")
+        self.currentlabel.setEnabled(False)
 
-        self.horizontalLayout_3.addWidget(self.repeat_current_node_chance)
+        self.horizontalLayout_3.addWidget(self.currentlabel)
 
-        self.label_15 = QLabel(self.groupBox)
-        self.label_15.setObjectName(u"label_15")
+        self.currentslider = QSlider(self.groupBox)
+        self.currentslider.setObjectName(u"currentslider")
+        self.currentslider.setEnabled(False)
+        self.currentslider.setMinimum(1)
+        self.currentslider.setMaximum(100)
+        self.currentslider.setOrientation(Qt.Horizontal)
 
-        self.horizontalLayout_3.addWidget(self.label_15)
+        self.horizontalLayout_3.addWidget(self.currentslider)
+
+        self.currentpercentage = QLabel(self.groupBox)
+        self.currentpercentage.setObjectName(u"currentpercentage")
+        self.currentpercentage.setEnabled(False)
+
+        self.horizontalLayout_3.addWidget(self.currentpercentage)
 
 
         self.gridLayout_2.addLayout(self.horizontalLayout_3, 7, 1, 1, 2)
@@ -103,25 +113,35 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.repeat_previous_note = QComboBox(self.groupBox)
+        self.repeat_previous_note = QCheckBox(self.groupBox)
         self.repeat_previous_note.setObjectName(u"repeat_previous_note")
 
         self.horizontalLayout_2.addWidget(self.repeat_previous_note)
 
-        self.label_14 = QLabel(self.groupBox)
-        self.label_14.setObjectName(u"label_14")
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.horizontalLayout_2.addWidget(self.label_14)
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_4)
 
-        self.repeat_previous_note_chance = QLineEdit(self.groupBox)
-        self.repeat_previous_note_chance.setObjectName(u"repeat_previous_note_chance")
+        self.previouslabel = QLabel(self.groupBox)
+        self.previouslabel.setObjectName(u"previouslabel")
+        self.previouslabel.setEnabled(False)
 
-        self.horizontalLayout_2.addWidget(self.repeat_previous_note_chance)
+        self.horizontalLayout_2.addWidget(self.previouslabel)
 
-        self.label_16 = QLabel(self.groupBox)
-        self.label_16.setObjectName(u"label_16")
+        self.previousslider = QSlider(self.groupBox)
+        self.previousslider.setObjectName(u"previousslider")
+        self.previousslider.setEnabled(False)
+        self.previousslider.setMinimum(1)
+        self.previousslider.setMaximum(100)
+        self.previousslider.setOrientation(Qt.Horizontal)
 
-        self.horizontalLayout_2.addWidget(self.label_16)
+        self.horizontalLayout_2.addWidget(self.previousslider)
+
+        self.previouspercentage = QLabel(self.groupBox)
+        self.previouspercentage.setObjectName(u"previouspercentage")
+        self.previouspercentage.setEnabled(False)
+
+        self.horizontalLayout_2.addWidget(self.previouspercentage)
 
 
         self.gridLayout_2.addLayout(self.horizontalLayout_2, 8, 1, 1, 2)
@@ -141,15 +161,15 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.notes_rests_slider, 0, 1, 1, 1)
 
-        self.percent_rests = QLabel(self.groupBox)
-        self.percent_rests.setObjectName(u"percent_rests")
-
-        self.gridLayout.addWidget(self.percent_rests, 0, 3, 1, 1)
-
         self.percent_notes = QLabel(self.groupBox)
         self.percent_notes.setObjectName(u"percent_notes")
 
         self.gridLayout.addWidget(self.percent_notes, 0, 0, 1, 1)
+
+        self.percent_rests = QLabel(self.groupBox)
+        self.percent_rests.setObjectName(u"percent_rests")
+
+        self.gridLayout.addWidget(self.percent_rests, 0, 3, 1, 1)
 
 
         self.gridLayout_2.addLayout(self.gridLayout, 6, 1, 1, 2)
@@ -183,27 +203,17 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.label_4, 3, 0, 1, 1)
 
-        self.tempo = QLineEdit(self.groupBox)
-        self.tempo.setObjectName(u"tempo")
-
-        self.gridLayout_2.addWidget(self.tempo, 5, 1, 1, 1)
-
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.time_enumerator = QComboBox(self.groupBox)
         self.time_enumerator.setObjectName(u"time_enumerator")
+        self.time_enumerator.setStyleSheet(u"")
 
         self.verticalLayout.addWidget(self.time_enumerator)
 
-        self.line = QFrame(self.groupBox)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.HLine)
-        self.line.setFrameShadow(QFrame.Sunken)
-
-        self.verticalLayout.addWidget(self.line)
-
         self.time_denominator = QComboBox(self.groupBox)
         self.time_denominator.setObjectName(u"time_denominator")
+        self.time_denominator.setStyleSheet(u"")
 
         self.verticalLayout.addWidget(self.time_denominator)
 
@@ -230,15 +240,24 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.label_11, 8, 0, 1, 1)
 
-        self.no_of_repeats = QLineEdit(self.groupBox)
-        self.no_of_repeats.setObjectName(u"no_of_repeats")
-
-        self.gridLayout_2.addWidget(self.no_of_repeats, 1, 1, 1, 1)
-
         self.no_of_voices = QSpinBox(self.groupBox)
         self.no_of_voices.setObjectName(u"no_of_voices")
 
         self.gridLayout_2.addWidget(self.no_of_voices, 2, 1, 1, 1)
+
+        self.tempo = QSpinBox(self.groupBox)
+        self.tempo.setObjectName(u"tempo")
+        self.tempo.setMinimum(12)
+        self.tempo.setMaximum(200)
+        self.tempo.setValue(60)
+
+        self.gridLayout_2.addWidget(self.tempo, 5, 1, 1, 1)
+
+        self.no_of_repeats = QSpinBox(self.groupBox)
+        self.no_of_repeats.setObjectName(u"no_of_repeats")
+        self.no_of_repeats.setMinimum(1)
+
+        self.gridLayout_2.addWidget(self.no_of_repeats, 1, 1, 1, 1)
 
 
         self.gridLayout_3.addLayout(self.gridLayout_2, 0, 0, 1, 1)
@@ -267,16 +286,9 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(shortcut)
         self.label_7.setBuddy(self.output_filename)
         self.label.setBuddy(self.composition_mode)
-        self.label_13.setBuddy(self.repeat_current_node_chance)
-        self.label_5.setBuddy(self.tempo)
-        self.label_14.setBuddy(self.repeat_previous_note_chance)
         self.label_3.setBuddy(self.no_of_voices)
         self.percent_notes.setBuddy(self.notes_rests_slider)
-        self.label_17.setBuddy(self.tempo)
-        self.label_2.setBuddy(self.no_of_repeats)
-        self.label_12.setBuddy(self.repeat_current_note)
         self.label_4.setBuddy(self.key)
-        self.label_11.setBuddy(self.repeat_previous_note)
 #endif // QT_CONFIG(shortcut)
 
         self.retranslateUi(MainWindow)
@@ -288,24 +300,26 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Compunist", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Compunist score settings", None))
         self.label_7.setText(QCoreApplication.translate("MainWindow", u"Output filename", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Compunist mode", None))
-        self.label_13.setText(QCoreApplication.translate("MainWindow", u"chance", None))
-        self.label_15.setText(QCoreApplication.translate("MainWindow", u"%", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Template", None))
+        self.repeat_current_note.setText("")
+        self.currentlabel.setText(QCoreApplication.translate("MainWindow", u"Chance", None))
+        self.currentpercentage.setText(QCoreApplication.translate("MainWindow", u"0%", None))
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"Tempo", None))
         self.open_file_explorer.setText(QCoreApplication.translate("MainWindow", u"...", None))
-        self.label_14.setText(QCoreApplication.translate("MainWindow", u"chance", None))
-        self.label_16.setText(QCoreApplication.translate("MainWindow", u"%", None))
+        self.repeat_previous_note.setText("")
+        self.previouslabel.setText(QCoreApplication.translate("MainWindow", u"Chance", None))
+        self.previouspercentage.setText(QCoreApplication.translate("MainWindow", u"0%", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Number of voices", None))
-        self.percent_rests.setText(QCoreApplication.translate("MainWindow", u"% rests", None))
         self.percent_notes.setText(QCoreApplication.translate("MainWindow", u"% notes", None))
+        self.percent_rests.setText(QCoreApplication.translate("MainWindow", u"% rests", None))
         self.label_17.setText(QCoreApplication.translate("MainWindow", u"bpm", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Number of series repeats", None))
-        self.label_12.setText(QCoreApplication.translate("MainWindow", u"Repeat current note?", None))
+        self.label_12.setText(QCoreApplication.translate("MainWindow", u"Repeat current note", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"Key signature", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Time signature", None))
         self.label_8.setText(QCoreApplication.translate("MainWindow", u"Notes/rests balance", None))
         self.label_18.setText("")
-        self.label_11.setText(QCoreApplication.translate("MainWindow", u"Repeat previous note?", None))
+        self.label_11.setText(QCoreApplication.translate("MainWindow", u"Repeat previous note", None))
         self.GenerateScore.setText(QCoreApplication.translate("MainWindow", u"Generate score", None))
     # retranslateUi
 
